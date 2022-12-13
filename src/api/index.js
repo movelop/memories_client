b8eb946ae9a3c1228842cf5543d23a0c30847bf2
Babalola Muhammad <barmilitary@yahoo.com>
2022-12-13 11:34:57 +0100

    moved server to render

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const API = axios.create({ baseURL: 'https://mo-memories.herokuapp.com' });
+const API = axios.create({ baseURL: 'https://memories-12ja.onrender.com' });
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('profile')) {
